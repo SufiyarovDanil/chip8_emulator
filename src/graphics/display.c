18d5c8c56f6a168d1f785d7b0020b3a720b09bd5
SufiyarovDanil <danil-sufiyarov@mail.ru>
2023-09-15 17:39:02 +0500

    imlpement functions

--- src/graphics/display.c
+++ src/graphics/display.c
@@ -3,17 +3,18 @@
 
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
+#define DISPLAY_SIZE DISPLAY_WIDTH * DISPLAY_HEIGHT
 
 
 struct display_s {
-	byte pixels[DISPLAY_WIDTH * DISPLAY_HEIGHT];
+	byte pixels[DISPLAY_SIZE];
 };
 
 
 display_t* display_new() {
 	display_t* const display = (display_t*)malloc(sizeof(display_t));
 
-	memset(display->pixels, 0, DISPLAY_WIDTH * DISPLAY_HEIGHT);
+	memset(display->pixels, 0, DISPLAY_SIZE);
 
 	return display;
 }
@@ -25,4 +26,31 @@ void display_kill(display_t* const self) {
 	}
 
 	free(self);
+}
+
+
+byte display_read_pixel(const display_t* const self, const word addr) {
+	if (!self || addr >= DISPLAY_SIZE) {
+		return 0;
+	}
+
+	return self->pixels[addr];
+}
+
+
+void display_write_pixel(display_t* const self, const word addr, const byte value) {
+	if (!self || addr >= DISPLAY_SIZE) {
+		return;
+	}
+
+	self->pixels[addr] = value;
+}
+
+
+word display_get_size(const display_t* const self) {
+	if (!self) {
+		return 0;
+	}
+
+	return DISPLAY_SIZE;
 }
--- src/graphics/display.h
+++ src/graphics/display.h
@@ -11,8 +11,10 @@ display_t* display_new();
 /* display destructor */
 void display_kill(display_t* const self);
 
-void display_read_pixel(const display_t* const self, const byte value);
+byte display_read_pixel(const display_t* const self, const word addr);
 
 void display_write_pixel(display_t* const self, const word addr, const byte value);
+
+word display_get_size(const display_t* const self);
 
 #endif // DISPLAY_H
